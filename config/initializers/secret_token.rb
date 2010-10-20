6d598d102a5d92974b932f7239eba343abcd6440
nathan <nathan@purpose.com>
2010-10-19 23:00:47 -0400

    test3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalpins::Application.config.secret_token = '3600bd4761c93557244a4838a6a3a7ac538f3c318e18e2632b95f4cf7b5a3dc0a1b3eaf1b033fb1e7750b07a7c62e4e02dd54659c944c7487af614c87faa5b67'
